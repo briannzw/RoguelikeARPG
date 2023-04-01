9628da49209857c3cc3435b3bd72f1fea16e158a
Brian Wijaya <70004754+briannzw@users.noreply.github.com>
2023-04-01 21:17:25 +0700

    Connect Scenes

---
+++ Assets/Scripts/Feature/SceneLoader/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
